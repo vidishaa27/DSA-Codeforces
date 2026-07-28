2cf5dda55f4ae0086523bd45fba57d35522a02df
Vidishaa Deo <vidishaa2709deo@gmail.com>
2026-07-28 19:31:34 +0530

    Day - 1

--- CakeLeveling.java
+++ CakeLeveling.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class CakeLeveling {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
