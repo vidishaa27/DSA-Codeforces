3745ad75bc15cae94907c2b5f70561000930e488
Vidishaa Deo <vidishaa2709deo@gmail.com>
2026-07-30 17:10:22 +0530

    Day - 2

---
+++ DivideAndConquer.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class DivideAndConquer {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            if (x % y == 0) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+            
+        }
+    }
+}
